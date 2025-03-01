52c6bcfc42d99c4688693f35bf631c6f71294f63
ByteTrailsHub <bytetrail24@gmail.com>
2025-03-01 22:04:30 +0700

    Send an email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "8277a7c63ede4da39277c99ffa4f1af5"
 url = ("https://newsapi.org/v2/top-headlines?coun"
@@ -12,6 +13,10 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article title and ddescriptions
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
