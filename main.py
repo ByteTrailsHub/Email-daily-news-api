c1494bb27b10ffae2eaefa5031bfe19e19492b64
ByteTrailsHub <bytetrail24@gmail.com>
2025-03-01 22:15:54 +0700

    Add links, specify language, limit news, add subject

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from send_email import send_email
 api_key = "8277a7c63ede4da39277c99ffa4f1af5"
 url = ("https://newsapi.org/v2/top-headlines?coun"
        "try=us&category=business&apiKey=8277a7"
-       "c63ede4da39277c99ffa4f1af5")
+       "c63ede4da39277c99ffa4f1af5&language=en")
 
 #make a request
 request = requests.get(url)
@@ -12,11 +12,13 @@ request = requests.get(url)
 #Get a dictionary with data
 content = request.json()
 
-# Access the article title and ddescriptions
+# Access the article title and descriptions
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+        body = ("Subject: Today's news" + "\n" + body
+                + article["title"] + "\n" + str(article["description"])
+                + "\n" + article["url"] + 2*"\n")
 
 body = body.encode("utf-8")
 send_email(message=body)
